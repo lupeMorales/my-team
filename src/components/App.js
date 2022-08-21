8728718d18a12aef8e2ae0b1e85ee2c1dfd302c3
lupeMorales <morales.lupegmail.com>
2022-08-21 18:12:21 +0200

    add html and css basic structure

---
+++ src/components/App.js
@@ -0,0 +1,55 @@
+import "../styles/App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="header">
+        <h1 className="header__title">the creative crew</h1>
+        <article>
+          <h2 className="header__subtitle">who we are</h2>
+          <p className="header__text">
+            We are team of creatively diverse. drive. innovative indivuduals
+            working in various locations from de world.
+          </p>
+        </article>
+      </header>
+      <main className="main">
+        <section className="main__container">
+          <article>
+            <h4 className="job"></h4>
+            <img src="" alt="" />
+            <h3 className="name"></h3>
+          </article>
+          <article>
+            <h4 className="job"></h4>
+            <img src="" alt="" />
+            <h3 className="name"></h3>
+          </article>
+          <article>
+            <h4 className="job"></h4>
+            <img src="" alt="" />
+            <h3 className="name"></h3>
+          </article>
+          <article>
+            <h4 className="job"></h4>
+            <img src="" alt="" />
+            <h3 className="name"></h3>
+          </article>
+          <article>
+            <h4 className="job"></h4>
+            <img src="" alt="" />
+            <h3 className="name"></h3>
+          </article>
+          <article>
+            <h4 className="job"></h4>
+            <img src="" alt="" />
+            <h3 className="name"></h3>
+          </article>
+        </section>
+      </main>
+      <footer className="footer"></footer>
+    </div>
+  );
+}
+
+export default App;
