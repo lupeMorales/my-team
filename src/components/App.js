8bcfe0bdea266883e18883214b43eb34573c39dd
lupeMorales <morales.lupegmail.com>
2022-08-21 22:03:20 +0200

    add grid layout

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,23 @@
 import "../styles/App.scss";
+import { useState } from "react";
+import dataMembers from "../data/members.json";
 
 function App() {
+  const [members, setMembers] = useState(dataMembers);
+
+  const renderMembers = () => {
+    return members.map((member, index) => {
+      return (
+        <li className="main__card" key={index}>
+          <div className="content">
+            <h4 className="job">{member.job}</h4>
+            <img className="img" src={member.img} alt="" />
+            <h3 className="name">{member.name}</h3>
+          </div>
+        </li>
+      );
+    });
+  };
   return (
     <div className="App">
       <header className="header">
@@ -8,43 +25,14 @@ function App() {
         <article>
           <h2 className="header__subtitle">who we are</h2>
           <p className="header__text">
-            We are team of creatively diverse. drive. innovative indivuduals
+            We are team of creatively diverse. drive. Innovative individuals
             working in various locations from de world.
           </p>
         </article>
       </header>
       <main className="main">
-        <section className="main__container">
-          <article>
-            <h4 className="job"></h4>
-            <img src="" alt="" />
-            <h3 className="name"></h3>
-          </article>
-          <article>
-            <h4 className="job"></h4>
-            <img src="" alt="" />
-            <h3 className="name"></h3>
-          </article>
-          <article>
-            <h4 className="job"></h4>
-            <img src="" alt="" />
-            <h3 className="name"></h3>
-          </article>
-          <article>
-            <h4 className="job"></h4>
-            <img src="" alt="" />
-            <h3 className="name"></h3>
-          </article>
-          <article>
-            <h4 className="job"></h4>
-            <img src="" alt="" />
-            <h3 className="name"></h3>
-          </article>
-          <article>
-            <h4 className="job"></h4>
-            <img src="" alt="" />
-            <h3 className="name"></h3>
-          </article>
+        <section>
+          <ul className="main__container">{renderMembers()}</ul>
         </section>
       </main>
       <footer className="footer"></footer>
